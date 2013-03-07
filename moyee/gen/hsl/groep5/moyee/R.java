44a8f9b24d6e98b1dc81b69534114abe87cd580a
shamanou <mandarijnopw8@gmail.com>
2013-03-07 15:13:17 +0100

    Laatste versie

--- moyee/gen/hsl/groep5/moyee/R.java
+++ moyee/gen/hsl/groep5/moyee/R.java
@@ -24,9 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int progressBar1=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int LinearLayout1=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int pager=0x7f080002;
+        public static final int welcome_message=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -40,7 +41,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int welcome_message=0x7f050002;
+        public static final int welcome_option1=0x7f050003;
+        public static final int welcome_option2=0x7f050004;
+        public static final int welcome_option3=0x7f050005;
     }
     public static final class style {
         /** 
