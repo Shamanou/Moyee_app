115ccc2dd489271e28c875f76036da99c96b9733
Robbert van der Hulst <robbertvanderhulst@hotmail.com>
2013-03-07 14:10:58 +0100

    Fragment Opties

--- moyee/gen/hsl/groep5/moyee/R.java
+++ moyee/gen/hsl/groep5/moyee/R.java
@@ -30,6 +30,9 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int fragmentoption1=0x7f030001;
+        public static final int fragmentoption2=0x7f030002;
+        public static final int fragmentoption3=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
