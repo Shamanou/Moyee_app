6e00be62459a2ee114493d4aa6fbe58c654e01d3
shamanou <mandarijnopw8@gmail.com>
2013-03-22 20:58:48 +0100

    poging om google maps te laten werken, niet gelukt wel verbeteringen aangebracht

--- moyee/src/hsl/groep5/moyee/FragmentOption1.java
+++ moyee/src/hsl/groep5/moyee/FragmentOption1.java
@@ -1,21 +1,22 @@
 package hsl.groep5.moyee;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 
-public class FragmentOption1 extends SupportMapFragment{
+public class FragmentOption1 extends Fragment{
 	public static final String ARG_SECTION_NUMBER = "section_number";
-	private GoogleMap mMap;
 	
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {	
-		View rootView = inflater.inflate(R.layout.fragmentoption1, null);
-		mMap = getMap();
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState); 
+		View rootView = inflater.inflate(R.layout.fragmentoption2, null);
+	    //Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "fonts/delicious-roman-webfont.ttf");  
+	    //((TextView) rootView).setTypeface(font);
 		return rootView;
 	}
 }
