0ecba81996040e89b279a6cad3248473ce36c2b7
shamanou <mandarijnopw8@gmail.com>
2013-03-28 09:31:46 +0100

    wijzeginen 28-03,2013

--- moyee/src/hsl/groep5/moyee/FragmentOption2.java
+++ moyee/src/hsl/groep5/moyee/FragmentOption2.java
@@ -1,14 +1,15 @@
 package hsl.groep5.moyee;
 
+import com.google.android.gms.maps.SupportMapFragment;
+
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class FragmentOption2 extends Fragment{
+public class FragmentOption2 extends SupportMapFragment{
 	public static final String ARG_SECTION_NUMBER = "section_number";	
 
 	@Override
