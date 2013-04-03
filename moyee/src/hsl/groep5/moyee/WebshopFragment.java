e86136732a6a83a36321fdd6e43f1b96ec0fa756
Mike <Mike@Mike-PC>
2013-04-03 17:32:32 +0200

    Merge issues...

--- moyee/src/hsl/groep5/moyee/WebshopFragment.java
+++ moyee/src/hsl/groep5/moyee/WebshopFragment.java
@@ -1,25 +1,12 @@
 package hsl.groep5.moyee;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import com.google.android.gms.maps.SupportMapFragment;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class WebshopFragment extends SupportMapFragment implements HttpAPIResult {
 	public static final String ARG_SECTION_NUMBER = "section_number";	
