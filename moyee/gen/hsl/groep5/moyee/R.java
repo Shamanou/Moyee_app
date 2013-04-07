083878128e98ec54bc1609b0d0db0c9f4d2d1082
Robbertvanderhulst <robbertvanderhulst@hotmail.com>
2013-04-07 20:41:49 +0200

    Groene kleur aan button

--- moyee/gen/hsl/groep5/moyee/R.java
+++ moyee/gen/hsl/groep5/moyee/R.java
@@ -25,9 +25,10 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int fairchain_coffee=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int marker=0x7f020002;
+        public static final int button=0x7f020000;
+        public static final int fairchain_coffee=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int marker=0x7f020003;
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f090000;
