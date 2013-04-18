9f45596d4cbafd9c221618bf0a5fa092e3202d2a
Mike <maikel.dj@gmail.com>
2013-04-18 14:03:26 +0200

    Added login dialog

--- moyee/src/hsl/groep5/moyee/MainActivity.java
+++ moyee/src/hsl/groep5/moyee/MainActivity.java
@@ -2,20 +2,29 @@ package hsl.groep5.moyee;
 
 import java.util.ArrayList;
 
+import android.app.Dialog;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
 import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 
-public class MainActivity  extends SherlockFragmentActivity {
+public class MainActivity extends SherlockFragmentActivity {
 	PagerAdapter mPagerAdapter;
 	ViewPager mViewPager;
 	ArrayList<Fragment> fragments = new ArrayList<Fragment>();
@@ -24,6 +33,7 @@ public class MainActivity  extends SherlockFragmentActivity {
 	WebshopFragment webshop = new WebshopFragment();
 	FragmentWelcome fragmentWelcome = new FragmentWelcome();
 	ActionBar actionbar;
+	ArrayList<Drawable> icons = new ArrayList<Drawable>();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +50,10 @@ public class MainActivity  extends SherlockFragmentActivity {
 		titles.add(getResources().getString(R.string.welcome_option2));
 		titles.add(getResources().getString(R.string.welcome_option3));
 		
+		icons.add(getResources().getDrawable(R.drawable.map));
+		icons.add(getResources().getDrawable(R.drawable.about));
+		icons.add(getResources().getDrawable(R.drawable.marker));
+		
 		fragments.add(gmaps);
 		fragments.add(fragmentWelcome);
 		fragments.add(webshop);
@@ -50,8 +64,12 @@ public class MainActivity  extends SherlockFragmentActivity {
 		
 		actionbar = getSupportActionBar();
 		
-	    PagerTabStrip strip = (PagerTabStrip)findViewById(R.id.tabStrip);
-	    strip.setTabIndicatorColor(getResources().getColor(R.color.green));
+		SharedPreferences settings = this.getSharedPreferences("settings", 0);
+		String name = settings.getString("name", "" );
+		String email = settings.getString("email", "" );
+//		if(name.equals("") || email.equals("")) {
+			showLogin();
+//		}
 	}
 	
 	@Override
@@ -59,5 +77,51 @@ public class MainActivity  extends SherlockFragmentActivity {
 	    MenuInflater inflater = getSupportMenuInflater();
 	    inflater.inflate(R.menu.main, menu);
 	    return true;
+	}
+	
+	public void showLogin () {
+		//set up dialog
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.login);
+        dialog.setTitle("Login");
+        dialog.setCancelable(false);
+        
+        
+        
+        SharedPreferences settings = getSharedPreferences("settings", 0);
+		String name = settings.getString("name", "" );
+		String email = settings.getString("email", "" );
+		
+		EditText txtName = (EditText) dialog.findViewById(R.id.username);
+		txtName.setText(name);
+		
+		TextView txtEmail = (TextView) dialog.findViewById(R.id.email);
+		txtEmail.setText(email);
+        
+        //set up button
+        Button btnSave = (Button) dialog.findViewById(R.id.save);
+        btnSave.setOnClickListener(new OnClickListener() {
+        @Override
+            public void onClick(View v) {
+	        	SharedPreferences settings = getSharedPreferences("settings", 0);
+	        	Editor edit = settings.edit();
+	        	EditText txtName = (EditText) dialog.findViewById(R.id.username);
+	        	edit.putString("name", txtName.getText().toString());
+	        	EditText txtEmail = (EditText) dialog.findViewById(R.id.email);
+	        	edit.putString("email", txtEmail.getText().toString());
+	        	edit.commit();
+	        	dialog.dismiss();
+            }
+        });
+        
+        Button btnSkip = (Button) dialog.findViewById(R.id.skip);
+        btnSkip.setOnClickListener(new OnClickListener() {
+        @Override
+            public void onClick(View v) {
+	        	dialog.dismiss();
+            }
+        });
+        //now that the dialog is set up, it's time to show it    
+        dialog.show();
 	}
 }
