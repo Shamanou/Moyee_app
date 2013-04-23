898d152358db79015cb016de3621d90b65327482
Mike <maikel.dj@gmail.com>
2013-04-23 21:25:20 +0200

    Added comments

--- moyee/src/hsl/groep5/moyee/Product.java
+++ moyee/src/hsl/groep5/moyee/Product.java
@@ -4,23 +4,24 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
-* @title Product Moyee Applicatie
-* @author Projectgroep 5
-* @param S1063848
-* @since 04-04-2013
-* @version 1.0
-*/
+ * @title Product object
+ * @author Maikel de Jong
+ * @param s1041697
+ * @since 04-04-2013
+ * @version 1.0
+ */
 
 public class Product {
 	private int id = 0;
 	private String title = "";
 	private String image = "";
-	private double price = 0 ;
+	private double price = 0;
 	private double size = 0;
 	private String description = "";
 	private int count = 0;
-	
+
 	public Product(JSONObject obj) {
+		// Extract data from JSONObject to Product
 		try {
 			this.id = obj.getInt("id");
 			this.title = obj.getString("title");
@@ -29,41 +30,44 @@ public class Product {
 			this.size = obj.getDouble("size");
 			this.description = obj.getString("description");
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-	
-	public String getTitle() {
-		return this.title;
-	}
-	public String getImageUrl() {
-		return this.image;
-	}
-	public double getPrice() {
-		return this.price;
-	}
-	public double getSize() {
-		return this.size;
-	}
-	public String getDescription() {
-		return this.description;
-	}
-	
-	public int getCount() {
-		return this.count;
-	}
-	
-	public void addCount() {
-		this.count++;
-	}
-	
-	public void subtractCount() {
-		if(this.count > 0) 
-			this.count--;
 	}
 
 	public int getId() {
 		return this.id;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public String getImageUrl() {
+		return this.image;
+	}
+
+	public double getPrice() {
+		return this.price;
+	}
+
+	public double getSize() {
+		return this.size;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public int getCount() {
+		return this.count;
+	}
+
+	public void addCount() {
+		this.count++;
+	}
+
+	public void subtractCount() {
+		if (this.count > 0)
+			this.count--;
 	}
 }
--- moyee/src/hsl/groep5/moyee/WebshopFragment.java
+++ moyee/src/hsl/groep5/moyee/WebshopFragment.java
@@ -26,120 +26,159 @@ import android.widget.Button;
 import android.widget.GridView;
 
 /**
-* @title Webshop Fragment Moyee Applicatie
-* @author Projectgroep 5
-* @param S1063848
-* @since 04-04-2013
-* @version 1.0
-*/
+ * @title Webshop Fragment Moyee Applicatie
+ * @author Aart van Amersfoort, Maikel de Jong
+ * @since 04-04-2013
+ * @version 1.0
+ */
 
-public class WebshopFragment extends SupportMapFragment implements
-		HttpAPIResult {
+public class WebshopFragment extends SupportMapFragment implements HttpAPIResult {
 	public static final String ARG_SECTION_NUMBER = "section_number";
 	GridView grid_main;
 	ImageAdapter imageAdapter = null;
 	Button button;
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		super.onCreateView(inflater, container, savedInstanceState);
-		Log.d("CHECK", "FragmentOption2 onCreateView()");
+		
+		// Inflate shopview layout
 		View rootView = inflater.inflate(R.layout.shopview, null);
-		grid_main = (GridView) rootView
-				.findViewById(R.id.welcome_option2_items);
-		this.imageAdapter = new ImageAdapter(this.getActivity());
+		
+		// Get gridview from view
+		grid_main = (GridView) rootView.findViewById(R.id.welcome_option2_items);
+		// Create and set ImageAdapter
+		imageAdapter = new ImageAdapter(this.getActivity());
 		grid_main.setAdapter(this.imageAdapter);
-		new HttpAPI(this)
-				.execute(getResources().getString(R.string.moyee_api_url) + "?get=products");
+		
+		// Create and execute new
+		new HttpAPI(this).execute(getResources().getString(R.string.moyee_api_url) + "?get=products");
 
+		// Get order button from view
 		button = (Button) rootView.findViewById(R.id.welcome_option2_button);
+		// Create new OnClickListener for order button
 		button.setOnClickListener(new OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
+				// Create new dialog
 				AlertDialog dialog = new AlertDialog.Builder(v.getContext()).create();
-			    dialog.setTitle("Confirmation");
-			    dialog.setMessage("Are you sure?");
-			    dialog.setCancelable(false);
-			    dialog.setButton(DialogInterface.BUTTON_POSITIVE, "Yes", new DialogInterface.OnClickListener() {
-			        public void onClick(DialogInterface dialog, int buttonId) {
-			        	try {
-			        		JSONObject json = new JSONObject();
-							json.put("action", "order");
-
-							SharedPreferences settings = WebshopFragment.this.getActivity().getSharedPreferences("settings", 0);
-							String userName = settings.getString("name", "" );
-							String userMail = settings.getString("email", "" );
-							
-							JSONObject user = new JSONObject();
-							user.put("name", userName);
-							user.put("email", userMail);
-							
-							json.put("user", user);
-							
-							JSONArray order = new JSONArray();
-			        	
-				        	for(int i = 0; i < WebshopFragment.this.imageAdapter.products.size(); i++)
-							{
-								Product p = WebshopFragment.this.imageAdapter.products.get(i);
-								Log.d(p.getTitle()+ "  "+ p.getCount(), " test");
+				dialog.setTitle("Confirmation");
+				dialog.setMessage("Are you sure?");
+				// Set cancelable false; user has to use the buttons
+				dialog.setCancelable(false);
+				// Set confirm button
+				dialog.setButton(DialogInterface.BUTTON_POSITIVE, "Yes", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,	int buttonId) {
+						// Load name and email form SharedPreferences
+						SharedPreferences settings = WebshopFragment.this.getActivity().getSharedPreferences("settings", 0);
+						String userName = settings.getString("name", "");
+						String userMail = settings.getString("email", "");
+						
+						// Check is both name and email are available
+						if (!userName.equals("") && !userMail.equals("")) {
+							try {
+								// Create new JSONObject for containing data to send to server
+								JSONObject json = new JSONObject();
+								json.put("action", "order");
 								
-								if(p.getCount() > 0) {
-									JSONObject product = new JSONObject();
-									product.put("id", p.getId());
-									product.put("count", p.getCount());
-									
-									order.put(product);
-								}
-							}
-				        	
-				        	json.put("products", order);
-				        	Log.d("JSON", json.toString());
-				        	
-				        	
-				        	new HttpAPI(WebshopFragment.this, 1).setParams(new BasicNameValuePair("order", json.toString())).execute(getResources().getString(R.string.moyee_api_url) + "?get=order");
-			        	} catch (JSONException e) {
-							e.printStackTrace();
-						}
-			        }
-			    });
-			    dialog.setButton(DialogInterface.BUTTON_NEGATIVE, "No", new DialogInterface.OnClickListener() {
-			        public void onClick(DialogInterface dialog, int buttonId) {}
-			    });
-			    dialog.setIcon(android.R.drawable.ic_dialog_alert);
-			    dialog.show();
-			}
+								// Create new JSONObject for user info
+								JSONObject user = new JSONObject();
+								user.put("name", userName);
+								user.put("email", userMail);
 
+								// Add user info to main object
+								json.put("user", user);
+
+								// Create new JSONArray for order info
+								JSONArray order = new JSONArray();
+
+								// Loop through all products
+								for (int i = 0; i < WebshopFragment.this.imageAdapter.products.size(); i++) {
+									// Store product[i] in object p
+									Product p = WebshopFragment.this.imageAdapter.products.get(i);
+									
+									// Check if product is ordered
+									if (p.getCount() > 0) {
+										// Create JSONObject for storing product order info
+										JSONObject product = new JSONObject();
+										// Add product id
+										product.put("id", p.getId());
+										// Add order count
+										product.put("count", p.getCount());
+										// Add product order info to order object
+										order.put(product);
+									}
+								}
+								
+								// Add order info to main object
+								json.put("products", order);
+
+								// Create a new async http request
+								new HttpAPI(WebshopFragment.this, 1)
+									// Set order parameter for posting
+									.setParams(new BasicNameValuePair("order", json.toString()))
+									// Execute request
+									.execute(getResources().getString(R.string.moyee_api_url) + "?get=order");
+							} catch (JSONException e) {
+								e.printStackTrace();
+							}
+						} else {
+
+						}
+					}
+
+				});
+				
+				// Set cancel button
+				dialog.setButton(DialogInterface.BUTTON_NEGATIVE, "No", new DialogInterface.OnClickListener() {
+					// Do nothing
+					public void onClick(DialogInterface dialog, int buttonId) {}
+				});
+				dialog.setIcon(android.R.drawable.ic_dialog_alert);
+				// Show dialog
+				dialog.show();
+			}
 		});
-		
 
 		return rootView;
 	}
 
 	@Override
 	public void onHttpResult(String result, int id) {
-		
 		JSONObject jsonObject = null;
 		try {
+			// Parse http result string to JSONObject
 			jsonObject = new JSONObject(result);
-			if(id == 0) {
-				// haal de array 'data' uit het object
-				JSONArray data = jsonObject.getJSONArray("data");
-				ArrayList<Product> products = new ArrayList<Product>();
-				for (int i = 0; i < data.length(); i++) {
-					// pak een item uit de array
-					JSONObject item = data.getJSONObject(i);
-					Product p = new Product(item);
-					products.add(p);
-				}
-				this.imageAdapter.setProducts(products);
-				grid_main.invalidateViews();
-			} else if(id == 1) {
-				
+			
+			// Check request id
+			switch(id) {
+				case 0:
+					// Get 'data' array form jsonObject
+					JSONArray data = jsonObject.getJSONArray("data");
+					
+					ArrayList<Product> products = new ArrayList<Product>();
+					
+					// Loop through 'data'
+					for (int i = 0; i < data.length(); i++) {
+						// Get JSONObject data[i]
+						JSONObject item = data.getJSONObject(i);
+						// Create new Product object from JSONObject item
+						Product p = new Product(item);
+						// Add product to list
+						products.add(p);
+					}
+					
+					// Set product list in adapter
+					imageAdapter.setProducts(products);
+					// Invalidate views for refresh
+					grid_main.invalidateViews();
+					break;
+				case 1:
+					// Show server confirmation
+					// @todo
+					break;			
 			}
 		} catch (JSONException e) {
-
 			e.printStackTrace();
 		}
 	}
