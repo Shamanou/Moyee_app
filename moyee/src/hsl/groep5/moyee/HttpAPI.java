a03c1d2a67c07763afe2e7bf7bd3cb19f6bf0730
Mike <maikel.dj@gmail.com>
2013-04-23 20:53:38 +0200

    Added comments

--- moyee/src/hsl/groep5/moyee/HttpAPI.java
+++ moyee/src/hsl/groep5/moyee/HttpAPI.java
@@ -4,84 +4,83 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
-
 import android.os.AsyncTask;
-import android.util.Log;
 
 /**
-* @title HTTP Api Moyee Applicatie
-* @author Projectgroep 5
-* @param S1063848
-* @since 04-04-2013
-* @version 1.0
-*/
+ * @title HttpAPI
+ * @author Maikel de Jong
+ * @param s1041697
+ * @since 04-04-2013
+ * @version 1.0
+ */
 
 public class HttpAPI extends AsyncTask<String, Integer, String> {
-	HttpAPIResult resultObject;
-	List<NameValuePair> params = new ArrayList<NameValuePair>();
-	int id = 0;
-	
-	public HttpAPI (HttpAPIResult resultObject) {
+	HttpAPIResult resultObject; // Calling object
+	List<NameValuePair> params = new ArrayList<NameValuePair>(); // Parameters
+	int id = 0; // Request id
+
+	public HttpAPI(HttpAPIResult resultObject) {
 		this.resultObject = resultObject;
 	}
-	
-	public HttpAPI (HttpAPIResult resultObject, int id) {
+
+	public HttpAPI(HttpAPIResult resultObject, int id) {
 		this.resultObject = resultObject;
 		this.id = id;
 	}
-	
+
 	public HttpAPI setParams(NameValuePair param) {
+		// Add parameter
 		params.add(param);
+		// Return this object
 		return this;
 	}
-	
+
 	@Override
 	protected String doInBackground(String... params) {
+		// Get url from params
 		String url = params[0];
-		
-		// Creating HTTP client
+
+		// Create http client
 		HttpClient httpClient = new DefaultHttpClient();
-		 
-		// Creating HTTP Post
+
+		// Create http post object
 		HttpPost httpPost = new HttpPost(url);
 		try {
+			// Set post parameters
 			httpPost.setEntity(new UrlEncodedFormEntity(this.params));
-		} catch (UnsupportedEncodingException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
 		}
+
 		String result = "";
-		// Making HTTP Request
+
 		try {
-		    HttpResponse response = httpClient.execute(httpPost);
-		    result = EntityUtils.toString(response.getEntity());
+			// Execute the http request; returns response;
+			HttpResponse response = httpClient.execute(httpPost);
+			// Parse http response to String
+			result = EntityUtils.toString(response.getEntity());
 		} catch (ClientProtocolException e) {
-		    // writing exception to log
-		    e.printStackTrace();
-		 
+			e.printStackTrace();
 		} catch (IOException e) {
-		    // writing exception to log
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
-		
+
 		return result;
 	}
-	
+
 	@Override
 	protected void onPostExecute(String result) {
 		super.onPostExecute(result);
-		// writing response to log
+		// Execute method on calling object
 		this.resultObject.onHttpResult(result, this.id);
 	}
-    
+
 }
--- moyee/src/hsl/groep5/moyee/HttpAPIResult.java
+++ moyee/src/hsl/groep5/moyee/HttpAPIResult.java
@@ -1,13 +1,13 @@
 package hsl.groep5.moyee;
 
 /**
-* @title HTTP API Resultaten Moyee Applicatie
-* @author Projectgroep 5
-* @param S1063848
-* @since 04-04-2013
-* @version 1.0
-*/
+ * @title HttpAPIResult interface
+ * @author Maikel de Jong
+ * @param s1041697
+ * @since 04-04-2013
+ * @version 1.0
+ */
 
 public interface HttpAPIResult {
-	public abstract void onHttpResult (String result, int id);
+	public abstract void onHttpResult(String result, int id);
 }
